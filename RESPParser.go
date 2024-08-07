9eb0f38a8fa1b3a39ceaf02c3f5029f25e9b7e0b
Jon Leach <leachj@github.com>
2024-08-07 20:43:09 +0000

    added generate methods
    Co-authored-by: Adam Flitney <adamflitney@users.noreply.github.com>

--- RESPParser.go
+++ RESPParser.go
@@ -7,6 +7,36 @@ import (
 	"strconv"
 )
 
+func GenerateRESP(value interface{}) (string, error) {
+	switch value := value.(type) {
+	case string:
+		return "+" + value + "\r\n", nil
+	case error:
+		return "-" + value.Error() + "\r\n", nil
+	case int64:
+		return ":" + strconv.FormatInt(value, 10) + "\r\n", nil
+	case []byte:
+		if value == nil {
+			return "$-1\r\n", nil
+		}
+		return "$" + strconv.Itoa(len(string(value))) + "\r\n" + string(value) + "\r\n", nil
+	case []interface{}:
+		if value == nil {
+			return "*-1\r\n", nil
+		}
+		result := "*" + strconv.Itoa(len(value)) + "\r\n"
+		for _, v := range value {
+			resp, err := GenerateRESP(v)
+			if err != nil {
+				return "", err
+			}
+			result += resp
+		}
+		return result, nil
+	}
+	return "", nil
+}
+
 // ParseRESP parses a Redis RESP message from the given reader.
 func ParseRESP(reader io.Reader) (interface{}, error) {
 	bufReader := bufio.NewReader(reader)
@@ -55,14 +85,14 @@ func parseInteger(reader *bufio.Reader) (int64, error) {
 	return strconv.ParseInt(line[:len(line)-2], 10, 64)
 }
 
-func parseBulkString(reader *bufio.Reader) (*string, error) {
+func parseBulkString(reader *bufio.Reader) ([]byte, error) {
 	line, err := reader.ReadString('\n')
 	if err != nil {
-		return new(string), err
+		return nil, err
 	}
 	length, err := strconv.Atoi(line[:len(line)-2])
 	if err != nil {
-		return new(string), err
+		return nil, err
 	}
 	if length == -1 {
 		return nil, nil
@@ -70,13 +100,13 @@ func parseBulkString(reader *bufio.Reader) (*string, error) {
 	buf := make([]byte, length+2)
 	_, err = io.ReadFull(reader, buf)
 	if err != nil {
-		return new(string), err
+		return nil, err
 	}
-	result := string(buf[:len(buf)-2])
-	return &result, nil
+	result := buf[:len(buf)-2]
+	return result, nil
 }
 
-func parseArray(reader *bufio.Reader) (*[]interface{}, error) {
+func parseArray(reader *bufio.Reader) ([]interface{}, error) {
 	line, err := reader.ReadString('\n')
 	if err != nil {
 		return nil, err
@@ -97,5 +127,5 @@ func parseArray(reader *bufio.Reader) (*[]interface{}, error) {
 		array[i] = value
 	}
 	result := array
-	return &result, nil
+	return result, nil
 }
--- RESPParser_test.go
+++ RESPParser_test.go
@@ -2,63 +2,89 @@ package parser
 
 import (
 	"errors"
+	"reflect"
 	"strings"
 	"testing"
 )
 
 func TestParseRESP(t *testing.T) {
+
+	nilByteArray := []byte(nil)
+	nilInterfaceArray := []interface{}(nil)
+
 	tests := []struct {
-		name     string
-		input    string
-		expected interface{}
+		name   string
+		resp   string
+		native interface{}
 	}{
 		{
-			name:     "Simple String",
-			input:    "+OK\r\n",
-			expected: "OK",
+			name:   "Simple String",
+			resp:   "+OK\r\n",
+			native: "OK",
 		},
 		{
-			name:     "Error",
-			input:    "-Error message\r\n",
-			expected: errors.New("Error message"),
+			name:   "Error",
+			resp:   "-Error message\r\n",
+			native: errors.New("Error message"),
 		},
 		{
-			name:     "Integer",
-			input:    ":1000\r\n",
-			expected: int64(1000),
+			name:   "Integer",
+			resp:   ":1000\r\n",
+			native: int64(1000),
 		},
 		{
-			name:     "Bulk String",
-			input:    "$6\r\nfoobar\r\n",
-			expected: "foobar",
+			name:   "Bulk String",
+			resp:   "$6\r\nfoobar\r\n",
+			native: []byte("foobar"),
 		},
 		{
-			name:     "Array",
-			input:    "*2\r\n$3\r\nfoo\r\n$3\r\nbar\r\n",
-			expected: []interface{}{"foo", "bar"},
+			name:   "Array",
+			resp:   "*2\r\n$3\r\nfoo\r\n$3\r\nbar\r\n",
+			native: []interface{}{[]byte("foo"), []byte("bar")},
 		},
 		{
-			name:     "Null Bulk String",
-			input:    "$-1\r\n",
-			expected: nil,
+			name:   "Empty Bulk String",
+			resp:   "$0\r\n\r\n",
+			native: []byte(""),
 		},
 		{
-			name:     "Null Array",
-			input:    "*-1\r\n",
-			expected: nil,
+			name:   "Null Bulk String",
+			resp:   "$-1\r\n",
+			native: nilByteArray,
+		},
+		{
+			name:   "Null Array",
+			resp:   "*-1\r\n",
+			native: nilInterfaceArray,
+		},
+		{
+			name:   "Empty Array",
+			resp:   "*0\r\n",
+			native: []interface{}{},
 		},
 	}
 
 	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			reader := strings.NewReader(tt.input)
+		t.Run("Parse "+tt.name, func(t *testing.T) {
+			reader := strings.NewReader(tt.resp)
 			result, err := ParseRESP(reader)
 			if err != nil {
 				t.Fatalf("unexpected error: %v", err)
 			}
 
-			if !equal(result, tt.expected) {
-				t.Errorf("expected %v, got %v", tt.expected, result)
+			if !equal(result, tt.native) {
+				t.Errorf("expected %v, got %v", tt.native, result)
+			}
+		})
+
+		t.Run("Generate "+tt.name, func(t *testing.T) {
+			result, err := GenerateRESP(tt.native)
+			if err != nil {
+				t.Fatalf("unexpected error: %v", err)
+			}
+
+			if !equal(result, tt.resp) {
+				t.Errorf("expected %v, got %v", tt.resp, result)
 			}
 		})
 	}
@@ -66,24 +92,24 @@ func TestParseRESP(t *testing.T) {
 
 func equal(a, b interface{}) bool {
 	switch a := a.(type) {
-	case *string:
+	case []byte:
 		if a == nil {
-			return b == nil
+			return reflect.ValueOf(b).IsNil()
 		}
-		return *a == b
+		return string(a) == string(b.([]byte))
 	case error:
 		b, ok := b.(error)
 		return ok && a.Error() == b.Error()
-	case *[]interface{}:
+	case []interface{}:
 		if a == nil {
-			return b == nil
+			return reflect.ValueOf(b).IsNil()
 		}
 		b, ok := b.([]interface{})
-		if !ok || len(*a) != len(b) {
+		if !ok || len(a) != len(b) {
 			return false
 		}
-		for i := range *a {
-			if !equal((*a)[i], b[i]) {
+		for i := range a {
+			if !equal((a)[i], b[i]) {
 				return false
 			}
 		}
